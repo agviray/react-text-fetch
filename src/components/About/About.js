2111493d068f5d9027625945abdf2ff78907fa3d
Allan Viray <allang.v.dev@gmail.com>
2023-05-01 16:59:33 -0500

    Update About and Instructions styles and structure

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,8 +4,8 @@ import './about.css';
 const About = () => {
   return (
     <div className="aboutContainer">
-      <h2>What is Text Fetch?</h2>
       <div>
+        <h2>What is Text Fetch?</h2>
         <p>
           Text Fetch is a tool used for inserting text templates in various text
           fields displayed in the Chrome browser.
--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -4,41 +4,43 @@ import './instructions.css';
 const Instructions = () => {
   return (
     <div className="instructionsContainer">
-      <h2>How it works</h2>
       <div>
+        <h2>How it works</h2>
         <ol>
-          <li>
-            <p>Fill in the Key field.</p>
-            {/* 
+          <div>
+            <li>
+              <p>Fill in the Key field.</p>
+              {/* 
 
             *** Implement Accordion component for this text? ***
 
             <p>The Key is case sensitive, and may consist
               of numbers, letters, or a comination of both. The Key must be 3 to
-              5 characters long.
+              4 characters long.
               </p> 
             */}
-          </li>
-          <li>
-            <p>Fill in the Template field.</p>
-          </li>
-          <li>
-            <p>Click Save.</p>
-          </li>
-          <li>
-            <p>
-              To use a saved template, go to a website and click in a text
-              field.
-            </p>
-          </li>
-          <li>
-            <p>
-              Type one of your saved keys in the field, and press Tab twice.
-            </p>
-          </li>
-          <li>
-            <p>The template associated with your key will be displayed.</p>
-          </li>
+            </li>
+            <li>
+              <p>Fill in the Template field.</p>
+            </li>
+            <li>
+              <p>Click Save.</p>
+            </li>
+            <li>
+              <p>
+                To use a saved template, go to a website and click in a text
+                field.
+              </p>
+            </li>
+            <li>
+              <p>
+                Type one of your saved keys in the field, and press Tab twice.
+              </p>
+            </li>
+            <li>
+              <p>The template associated with your key will be displayed.</p>
+            </li>
+          </div>
         </ol>
       </div>
     </div>
