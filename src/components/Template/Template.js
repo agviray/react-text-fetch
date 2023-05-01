1f5c2dfecfdeed646ff5a686438d376d3a025019
Allan Viray <allang.v.dev@gmail.com>
2023-05-01 13:38:32 -0500

    Edit Template styles
    - Edit other style files as necessary.
    - Add "Save" button.

--- src/components/Template/Template.js
+++ src/components/Template/Template.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import './template.css';
 
 const Template = () => {
   return (
-    <div>
+    <div className="templateContainer">
       <h3>Template</h3>
-      <textarea></textarea>
+      <textarea />
+      <div>
+        <span>Save</span>
+      </div>
     </div>
   );
 };
