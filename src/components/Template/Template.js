79a5613bcda33bc5a2e9496f993abd1676e9c643
Allan Viray <allang.v.dev@gmail.com>
2023-05-02 15:52:40 -0500

    Track Template changes with state

--- src/components/Template/Template.js
+++ src/components/Template/Template.js
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './template.css';
 
 const Template = () => {
+  const [templateText, setTemplateText] = useState('');
+
+  const handleChange = (text) => {
+    // - Set word limit to maximum of 30.
+    if (text.split(' ').length > 30) {
+      return;
+    }
+    setTemplateText(text);
+  };
+
   return (
     <div className="templateContainer">
       <h3>Template</h3>
-      <textarea />
+      <textarea
+        value={templateText}
+        onChange={(e) => handleChange(e.target.value)}
+        onFocus={(e) => e.target.select()}
+      />
     </div>
   );
 };
