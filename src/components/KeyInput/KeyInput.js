0290648463efb9922bb541886c05bd34cf09440c
Allan Viray <allang.v.dev@gmail.com>
2023-05-02 15:27:49 -0500

    Track Key changes with state and apply error message as needed

--- src/components/KeyInput/KeyInput.js
+++ src/components/KeyInput/KeyInput.js
@@ -1,11 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './keyInput.css';
 
 const KeyInput = () => {
+  const [key, setKey] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (text) => {
+    // - Set max key characters to 4.
+    if (text.length > 4) {
+      return;
+    }
+    setKey(text);
+  };
+
+  const handleInputBlur = (text) => {
+    // - Regular expression for only numbers and letters.
+    const regex = /^[A-Za-z0-9]*$/;
+    if (regex.test(text) === false) {
+      setErrorMessage('Key must only contain numbers or letters.');
+    } else if (text.length < 3) {
+      setErrorMessage('Key must be 3 to 4 characters.');
+    }
+  };
+
+  const handleInputFocus = (e) => {
+    e.target.select();
+    return errorMessage === '' ? null : setErrorMessage('');
+  };
+
   return (
     <div className="keyInputContainer">
       <h3>Key</h3>
-      <input type="text" />
+      <input
+        type="text"
+        value={key}
+        onFocus={(e) => handleInputFocus(e)}
+        onBlur={(e) => handleInputBlur(e.target.value)}
+        onChange={(e) => handleInputChange(e.target.value)}
+      />
+      {errorMessage === '' ? null : <p>{errorMessage}</p>}
     </div>
   );
 };
