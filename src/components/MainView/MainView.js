32ea77f81eeb21060041490a541b3fae22451edd
Allan Viray <allang.v.dev@gmail.com>
2023-05-02 13:00:46 -0500

    Get or set a key in localStorage when app first renders
    - If key (defined by STORED_KEY_TEMPLATE_PAIRS) is not found in
      localStorage, set up useEffect to store value of
      STORED_KEY_TEMPLATE_PAIRS as a key in localStorage.
    
    - If matching key is found, get data from that key's value (if any
      data available) and set it as value of state, keyTemplatePairs.

--- src/components/MainView/MainView.js
+++ src/components/MainView/MainView.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import KeyCollection from '../KeyCollection/KeyCollection';
 import KeyInput from '../KeyInput/KeyInput';
 import Template from '../Template/Template';
 import './mainView.css';
 
+const storedKeyTemplatePairs = 'storedKeyTemplatePairs';
+
 const MainView = () => {
+  const [keyTemplatePairs, setKeyTemplatePairs] = useState([]);
+
+  // - Initial load of stored key/template pairs.
+  // - This will either get stored data of key from localStorage, or set the initial
+  //   key value pair in localStorage.
+  useEffect(() => {
+    const storedPairs = JSON.parse(
+      localStorage.getItem(storedKeyTemplatePairs)
+    );
+    if (storedPairs) {
+      setKeyTemplatePairs([...storedPairs]);
+    } else {
+      localStorage.setItem(
+        storedKeyTemplatePairs,
+        JSON.stringify(keyTemplatePairs)
+      );
+    }
+  }, []);
+
   return (
     <div className="mainContainer">
       <div>
