aeca1d48969b82416ea14cd6d33226b545c436d2
Allan Viray <allang.v.dev@gmail.com>
2023-05-06 15:34:59 -0500

    Update Instructions contents

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -7,7 +7,7 @@ const About = () => {
       <div>
         <h2>What is Text Fetch?</h2>
         <p>
-          Text Fetch is tool used to create, stash, and fetch your text
+          Text Fetch is tool used to create, save, and fetch your text
           templates.
         </p>
       </div>
--- src/components/Instructions/Instructions.js
+++ src/components/Instructions/Instructions.js
@@ -1,47 +1,52 @@
 import React from 'react';
 import './instructions.css';
+import Accordion from '../Accordion/Accordion';
+
+const savingYourTextContent = {
+  heading: 'Saving and editing your text',
+  body: [
+    {
+      bodyHeading: 'Saving',
+      bodyText: [
+        'Fill in the Key and Template fields, and then click on the Save button.',
+        'Your Key/Template pair will now be saved.',
+      ],
+    },
+    {
+      bodyHeading: 'Editing and deleting',
+      bodyText: [
+        'To edit or delete a Key/Template plair, selecte a key name from the All Keys list.',
+        'The selected keys name and template will appear in their respective fields, along with additional button options.',
+        'Click the Edit button to enter an editing mode. You will now be able to edit the Key or Template.',
+        'Make your changes then save them by clicking the Save button.',
+        'Click the Delete button to delete a Key/Template pair.',
+        'A popup will appear, where you can choose to confirm or cancel your decision.',
+      ],
+    },
+  ],
+};
+
+const usingYourTextContent = {
+  heading: 'Using your text',
+  body: [
+    {
+      bodyText: [
+        'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eaque, provident.',
+        'Lorem ipsum dolor sit amet consectetur adipisicing.',
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi!',
+        'Lorem ipsum dolor sit amet consectetur.',
+      ],
+    },
+  ],
+};
 
 const Instructions = () => {
   return (
     <div className="instructionsContainer">
       <div>
         <h2>How it works</h2>
-        <ol>
-          <div>
-            <li>
-              <p>Fill in the Key field.</p>
-              {/* 
-
-            *** Implement Accordion component for this text? ***
-
-            <p>The Key is case sensitive, and may consist
-              of numbers, letters, or a comination of both. The Key must be 3 to
-              4 characters long.
-              </p> 
-            */}
-            </li>
-            <li>
-              <p>Fill in the Template field.</p>
-            </li>
-            <li>
-              <p>Click Save.</p>
-            </li>
-            <li>
-              <p>
-                To use a saved template, go to a website and click in a text
-                field.
-              </p>
-            </li>
-            <li>
-              <p>
-                Type one of your saved keys in the field, and press Tab twice.
-              </p>
-            </li>
-            <li>
-              <p>The template associated with your key will be displayed.</p>
-            </li>
-          </div>
-        </ol>
+        <Accordion accordionContent={savingYourTextContent} />
+        <Accordion accordionContent={usingYourTextContent} />
       </div>
     </div>
   );
