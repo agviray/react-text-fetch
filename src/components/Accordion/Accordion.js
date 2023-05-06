6b6f7cce69a7ac6a6eee1be6ace295327d90940b
Allan Viray <allang.v.dev@gmail.com>
2023-05-06 15:35:29 -0500

    Update Accordion.js content and styles, and implement in Instructions

--- src/components/Accordion/Accordion.js
+++ src/components/Accordion/Accordion.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './accordion.css';
 
-const Accordion = () => {
+const Accordion = ({ accordionContent }) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleIsOpen = (isOpenStatus) => {
+    return setIsOpen(!isOpenStatus);
+  };
+
   return (
-    <div>
-      <h1>Accordion</h1>
+    <div className="accordionContainer">
+      <div className="headingContainer" onClick={() => toggleIsOpen(isOpen)}>
+        <h3>{accordionContent.heading}</h3>
+        {isOpen ? <span>&#8722;</span> : <span>&#43;</span>}
+      </div>
+      <div className={`bodyContainer ${isOpen ? 'isOpen' : ''}`}>
+        {accordionContent.body.map(({ bodyHeading, bodyText }, index) => {
+          return (
+            <div key={index}>
+              {bodyHeading ? <h4>{bodyHeading}</h4> : null}
+              <ol>
+                {bodyText.map((text, index) => (
+                  <li key={index}>
+                    <p>{text}</p>
+                  </li>
+                ))}
+              </ol>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
